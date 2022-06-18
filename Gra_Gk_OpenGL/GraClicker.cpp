c4c33a8eef4efaf73215ce6bb9e1747559b376ee
MicWalczak <michwal323@student.polsl.pl>
2022-06-18 16:56:59 +0200

    mouse

--- Gra_Gk_OpenGL/GraClicker.cpp
+++ Gra_Gk_OpenGL/GraClicker.cpp
@@ -1,11 +1,21 @@
-
 #include <iostream>
 #include<GL/glut.h>
+#include <GLFW/glfw3.h>
 
-int x = rand() % 1000;
-int y = rand() % 1000;
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int x, y;
 int color = 255;
 int l = 0;
+void mous(int button, int state, int mousex, int mousey)
+{
+	if (button == GLUT_LEFT_BUTTON && stat == GLUT_DOWN)
+	{
+		x = mousex;
+		y = 480 - mousey;
+	}
+}
 class Pamiec 
 {
 private:
@@ -18,29 +28,24 @@ public:
 		this->x = x;
 		this->y = y;
 		this->length = length;
-
 }
 };
-
 void GLinit();
 void myDisplayFunc();
 void rysujKwadrat(int x1, int y1, int sidelength);
-int main()
 
+
+int main()
 {
 	srand(time(0));
-	
-	
-
+	GLFWwindow* window;
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Grafika Komputerowa",NULL, NULL);
+	glfwMakeContextCurrent(window);
 	glutInit(&__argc, __argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(1000, 1000);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Grafika Komputerowa");
-	
-
-	
-
 
 	GLinit();
 	
@@ -61,22 +66,20 @@ void GLinit()
 
 void myDisplayFunc() {
 	l++;
-	std::cout << l;
+	//std::cout << l;
+	std::cout << x << " : " << y<<" ";
 	if (l % 5 == 0)color = 0;
 	else { color = 255; }
-	x = rand() % 1000;
-	y = rand() % 1000;
+	x = x = rand() % 1000;
+	y = y = rand() % 1000;
 	if(l>2)
 	rysujKwadrat(x, y, 100);
 	
 	glFlush();
 }
 
-
 void rysujKwadrat(int x1, int y1, int sidelength) {
 	double halfside = sidelength / 2;
-
-	
 
 	glColor3d(255, 0, color);
 	glBegin(GL_POLYGON);
@@ -88,6 +91,7 @@ void rysujKwadrat(int x1, int y1, int sidelength) {
 
 	glEnd();
 }
+
 
 void gra() {
 
