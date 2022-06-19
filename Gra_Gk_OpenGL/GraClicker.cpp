335131c725d0c09cec7bf5f38a9c5838d41c9eb7
Rafekpro <codethegloobe@gmail.com>
2022-06-19 13:51:16 +0200

    kwadrat

--- Gra_Gk_OpenGL/GraClicker.cpp
+++ Gra_Gk_OpenGL/GraClicker.cpp
@@ -6,41 +6,78 @@
 #define SCREEN_HEIGHT 480
 
 int x, y;
+int Mousex, Mousey;
 int color = 255;
 int l = 0;
+void rysujKwadrat(int x1, int y1, int sidelength);
+void ZamalujKwadrat(int x1, int y1, int sidelength);
+
 void mous(int button, int state, int mousex, int mousey)
 {
 	if (button == GLUT_LEFT_BUTTON && stat == GLUT_DOWN)
 	{
-		x = mousex;
-		y = 480 - mousey;
+		Mousex = mousex;
+		Mousey = 480 - mousey;
 	}
 }
 class Pamiec 
 {
 private:
-	int x;
-	int y;
-	int length;
-	bool czyCzerwony;
+	int x=0;
+	int y=0;
+	int length=100;
+	bool czyCzerwony = false;
 public:
-	Pamiec(int x, int y, int length) {
+	Pamiec(int x, int y, int length, bool czyCzerwony) {
 		this->x = x;
 		this->y = y;
 		this->length = length;
+		this->czyCzerwony = czyCzerwony;
+
 }
+
+	void rysuj() 
+	{
+		if (this->czyCzerwony == true)color = 0;
+		else color = 255;
+		rysujKwadrat(this->x, this->y, this->length);
+}
+
+	void zamaluj() {
+		ZamalujKwadrat(this->g_x(), this->g_y(), 100);
+	}
+
+
+void s_x(int x)
+{
+	this->x = x;
+}
+int g_x()
+{
+	return this->x;
+}
+void s_y(int y)
+{
+	this->y = y;
+}
+int g_y()
+{
+	return this->y;
+}
+
 };
+
 void GLinit();
 void myDisplayFunc();
-void rysujKwadrat(int x1, int y1, int sidelength);
 
+Pamiec p1{ rand() % 1000,rand() % 1000,100,false };
+Pamiec p2{ rand() % 1000,rand() % 1000,100,true };
+Pamiec p3{ rand() % 1000,rand() % 1000,100,false };
+Pamiec p4{ rand() % 1000,rand() % 1000,100,false };
 
 int main()
 {
 	srand(time(0));
-	GLFWwindow* window;
-	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Grafika Komputerowa",NULL, NULL);
-	glfwMakeContextCurrent(window);
 	glutInit(&__argc, __argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(1000, 1000);
@@ -51,7 +88,15 @@ int main()
 	
 	
 	 glutDisplayFunc(myDisplayFunc);
-	
+
+	 
+	 p1.rysuj();
+
+	 p2.rysuj();
+	 
+	 p3.rysuj();
+
+	 p4.rysuj();
 	
 	glutMainLoop();
 }
@@ -66,15 +111,14 @@ void GLinit()
 
 void myDisplayFunc() {
 	l++;
-	//std::cout << l;
-	std::cout << x << " : " << y<<" ";
-	if (l % 5 == 0)color = 0;
-	else { color = 255; }
-	x = x = rand() % 1000;
-	y = y = rand() % 1000;
-	if(l>2)
-	rysujKwadrat(x, y, 100);
-	
+
+	std::cout << Mousex << " : " << Mousey << " " << std::endl;
+
+	if (l % 5 == 0) //ZAMIAST TEGO MA BYC SPRAWDZENIE CZY BYLO KLIKNIETE W KWADRAT TJ. X I Y  MYSZKI MIESZCZCZA SIE W PRZEDIALE KWADRATU
+	{
+		p1.zamaluj(); p1.s_x(rand() % 1000); p1.s_y(rand() % 1000); p1.rysuj();
+}
+
 	glFlush();
 }
 
@@ -82,6 +126,20 @@ void rysujKwadrat(int x1, int y1, int sidelength) {
 	double halfside = sidelength / 2;
 
 	glColor3d(255, 0, color);
+	glBegin(GL_POLYGON);
+
+	glVertex2d(x1 + halfside, y1 + halfside);
+	glVertex2d(x1 + halfside, y1 - halfside);
+	glVertex2d(x1 - halfside, y1 - halfside);
+	glVertex2d(x1 - halfside, y1 + halfside);
+
+	glEnd();
+}
+
+void ZamalujKwadrat(int x1, int y1, int sidelength) {
+	double halfside = sidelength / 2;
+
+	glColor3d(0,0, 0);
 	glBegin(GL_POLYGON);
 
 	glVertex2d(x1 + halfside, y1 + halfside);
